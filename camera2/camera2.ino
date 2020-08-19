1c1cd4ec12eed513fe8c3957a51b8e7774785c62
195cn <2259761058@qq.com>
2020-08-19 13:43:00 +0800

    ov2640 show on tft

---
+++ camera2/camera2.ino
@@ -0,0 +1,118 @@
+#include "SPI.h"
+#include "ILI9488.h"
+#include "esp_camera.h"
+#define CAMERA_MODEL_MAKERFABS
+#include "camera_pins.h"
+
+//SPI
+#define TFT_CS 15
+#define TFT_DC 33
+#define TFT_LED -1
+#define TFT_RST -1
+#define SPI_MOSI 13
+#define SPI_MISO 12
+#define SPI_SCK 14
+
+ILI9488 tft = ILI9488(TFT_CS, TFT_DC, TFT_RST);
+
+void setup()
+{
+
+    //camera config
+    camera_config_t config;
+    config.ledc_channel = LEDC_CHANNEL_0;
+    config.ledc_timer = LEDC_TIMER_0;
+    config.pin_d0 = Y2_GPIO_NUM;
+    config.pin_d1 = Y3_GPIO_NUM;
+    config.pin_d2 = Y4_GPIO_NUM;
+    config.pin_d3 = Y5_GPIO_NUM;
+    config.pin_d4 = Y6_GPIO_NUM;
+    config.pin_d5 = Y7_GPIO_NUM;
+    config.pin_d6 = Y8_GPIO_NUM;
+    config.pin_d7 = Y9_GPIO_NUM;
+    config.pin_xclk = XCLK_GPIO_NUM;
+    config.pin_pclk = PCLK_GPIO_NUM;
+    config.pin_vsync = VSYNC_GPIO_NUM;
+    config.pin_href = HREF_GPIO_NUM;
+    config.pin_sscb_sda = SIOD_GPIO_NUM;
+    config.pin_sscb_scl = SIOC_GPIO_NUM;
+    config.pin_pwdn = PWDN_GPIO_NUM;
+    config.pin_reset = RESET_GPIO_NUM;
+    config.xclk_freq_hz = 20000000;
+
+    config.pixel_format = PIXFORMAT_RGB565;
+    config.frame_size = FRAMESIZE_QVGA;
+    config.fb_count = 1;
+
+    // camera init
+    esp_err_t err = esp_camera_init(&config);
+    if (err != ESP_OK)
+    {
+        Serial.printf("Camera init failed with error 0x%x", err);
+        while (1)
+            ;
+    }
+    Serial.printf("Camera init Success");
+
+    sensor_t *s = esp_camera_sensor_get();
+    //initial sensors are flipped vertically and colors are a bit saturated
+    if (s->id.PID == OV2640_PID)
+    {
+        s->set_vflip(s, 1);       //flip it back
+        s->set_brightness(s, 1);  //up the blightness just a bit
+        s->set_saturation(s, -1); //lower the saturation
+    }
+    //drop down frame size for higher initial frame rate
+    s->set_framesize(s, FRAMESIZE_QVGA);
+
+#if defined(CAMERA_MODEL_M5STACK_WIDE)
+    Serial.println("CAMERA_MODEL_M5STACK_WIDE");
+    s->set_vflip(s, 1);
+    s->set_hmirror(s, 1);
+#endif
+}
+
+void loop(void)
+{
+    camera_fb_t *fb = NULL;
+
+    fb = esp_camera_fb_get();
+    if (!fb)
+    {
+        Serial.println("Camera capture failed");
+    }
+    Serial.println(fb->len);
+    Serial.println(fb->width);
+    Serial.println(fb->height);
+
+    drawRGBBitmap(fb->buf, fb->width, fb->height);
+
+    esp_camera_fb_return(fb);
+}
+
+void drawRGBBitmap(uint8_t *bitmap, int16_t w, int16_t h)
+{
+    tft.startWrite();
+    for (int16_t j = 0; j < h; j++)
+    {
+        for (int16_t i = 0; i < w; i++)
+        {
+            uint16_t temp = 0;
+            temp = bitmap[j * w * 2 + 2 * i] * 256 + bitmap[j * w * 2 + 2 * i + 1];
+            tft.writePixel(i, j, temp);
+        }
+    }
+    tft.endWrite();
+}
+
+void init()
+{
+    Serial.begin(115200);
+    Serial.println("ILI9488 Test!");
+
+    SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
+
+    tft.begin();
+    tft.setRotation(1);
+    tft.fillScreen(ILI9488_BLACK);
+}
--- readme.md
+++ readme.md
@@ -1,4 +1,48 @@
-# ILI9488 screen
+# Touch Screen Camera
 
-先搞定从sd卡读位图
+```c++
+/*
+Version:		V1.0
+Author:			Vincent
+Create Date:	2020/8/19
+Note:
+	
+*/
+```
+
+[toc]
+
+# OVERVIEW
+
+## TFT screen
+### ILI9488
+
+9488没有arduino下的官方库，根据github上的第三方驱动，修改了一个版本。可以实现Adafruit GFX库的基本功能：画点，线，几何图形，方向调整，位图显示等。S
+
+- /Project_Touch-Screen-Camra/ILI9488 ：ILI9488驱动
+- /Project_Touch-Screen-Camra/9488test：ILI9488屏幕测试，SD卡测试
+- /Project_Touch-Screen-Camra/SD2TFT ：从SD卡显示图片
+
+
+
+## Touch Screen
+
+### NS2009
+
+NS2009是一款I2C协议的电阻屏驱动IC，输出x，y，z1，z2四个轴的12位数据。
+
+### FT6236
+
+FT6236是一款I2C协议的电容屏驱动IC，直接输出滤波后的坐标值。
+
+- /Project_Touch-Screen-Camra/touch_screen_driver_example/ft6236 :FT6236驱动示范
+- /Project_Touch-Screen-Camra/touch_screen_driver_example/ns2009 ：NS2009驱动示范
+- /Project_Touch-Screen-Camra/touch_screen_driver_example/i2c_scanner ：I2C扫描
+- /Project_Touch-Screen-Camra/touch_draw ：触屏画图测试，兼容NS2009和FT6236
+
+
+
+## Camera
+
+### OV2640
 
